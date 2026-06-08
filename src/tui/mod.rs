66ab43dbdd55e90bb4a4345a0747907a18e30727
GabrielPontess <gabrielpontescontato1@gmail.com>
2026-06-08 13:39:10 -0300

    feat: struct tui mod

---
+++ src/tui/mod.rs
@@ -0,0 +1,53 @@
+use std::io::{self, Stdout};
+use crossterm::{execute, terminal::{enable_raw_mode, disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen}};
+use ratatui::{prelude::*, widgets::*};
+
+pub struct TuiTerminal {
+    terminal: Terminal<CrosstermBackend<Stdout>>,
+}
+
+impl TuiTerminal {
+    pub fn new() -> io::Result<Self> {
+        enable_raw_mode()?;
+        let mut stdout = io::stdout();
+        execute!(stdout, EnterAlternateScreen)?;
+        let backend = CrosstermBackend::new(stdout);
+        let terminal = Terminal::new(backend)?;
+        Ok(Self { terminal })
+    }
+
+    pub fn draw(&mut self, progress: f64, logs: &[String]) -> io::Result<()> {
+        self.terminal.draw(|f| {
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints([
+                    Constraint::Length(3),
+                    Constraint::Min(5),
+                    Constraint::Length(8),
+                ])
+                .split(f.size());
+
+            let gauge_chuncks = Gauge::default()
+                .block(Block::default().title(" Progresso Global x9 ").borders(Borders::ALL))
+                .gauge_style(Style::default().fg(Color::Green).bg(Color::Black))
+                .ratio(progress.clamp(0.0, 1.0));
+            f.render_widget(gauge_chuncks, chunks[0]);
+
+            let items_progress = [ListItem::new("Nenhum arquivo sendo transferido no momento...")];
+            let list = List::new(items_progress).block(Block::default().title(" Transferências Ativas ").borders(Borders::ALL));
+            f.render_widget(list, chunks[1]);
+
+            let log_items: Vec<ListItem> = logs.iter().map(|log| ListItem::new(log.as_str())).collect();
+            let log_list = List::new(log_items).block(Block::default().title(" Logs em Tempo Real ").borders(Borders::ALL));
+            f.render_widget(log_list, chunks[2]);
+        })?;
+        Ok(())
+    }
+}
+
+impl Drop for TuiTerminal {
+    fn drop(&mut self) {
+        let _ = disable_raw_mode();
+        let _ = execute!(self.terminal.backend_mut(), LeaveAlternateScreen);
+    }
+}
